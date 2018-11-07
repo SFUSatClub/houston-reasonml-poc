40376fca06e2860c8f3256ca4dc3ee7a82c4e30d
Alireza Alidousti <alireza.eva.u23@gmail.com>
2018-11-06 18:03:43 -0800

    initializes a mocked port in the index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+/**
+ * SerialPort
+ */
+const SerialPort = require("@serialport/stream");
+const Readline = require("@serialport/parser-readline");
+const MockBinding = require("@serialport/binding-mock");
+
+SerialPort.Binding = MockBinding;
+
+MockBinding.createPort("/dev/ROBOT", { echo: true, record: true });
+const port = new SerialPort("/dev/ROBOT");
+port.pipe(new Readline({ delimiter: "\n" }));
+
+/**
+ * GraphQL
+ */
 const { GraphQLServer, PubSub } = require("graphql-yoga");
 
 const pubsub = new PubSub();
